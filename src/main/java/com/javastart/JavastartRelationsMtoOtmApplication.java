a6e787bf2218dbb5768e424762bac64b38475a19
Krzysztof <krzubrycki@gmail.com>
2022-02-17 14:14:12 +0100

    created project

---
+++ src/main/java/com/javastart/JavastartRelationsMtoOtmApplication.java
@@ -0,0 +1,13 @@
+package com.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastartRelationsMtoOtmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavastartRelationsMtoOtmApplication.class, args);
+    }
+
+}
